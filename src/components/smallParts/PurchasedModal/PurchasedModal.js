78b136437521416fd3609f7e56dd7e79170d7f26
priusGit <marcinszaro@gmail.com>
2021-05-13 18:36:47 +0200

    disign: Added icons to modals after purchase
    - New animated icons added to modal popping after purchase
    
    - Code clean in PurchasedModal

--- src/components/smallParts/PurchasedModal/PurchasedModal.js
+++ src/components/smallParts/PurchasedModal/PurchasedModal.js
@@ -5,6 +5,8 @@ import classes from "./PurchasedModal.module.css";
 import Spinner from "../Spinner/Spinner";
 import Auxi from "../../../hoc/Auxi";
 import { NavLink } from "react-router-dom";
+import pan from "../../../assets/svg/cooking.svg";
+import nosignal from "../../../assets/svg/no-signal.svg";
 class PurchasedModal extends Component {
   render() {
     let modalConent;
@@ -17,6 +19,11 @@ class PurchasedModal extends Component {
             <h1 className={classes.ModalH}>Garnki w ruch!</h1>
             <p>twoje zamówienie zostało przyjęte przez restaurację</p>
             <p>Wysyłka za około: 45 minut</p>
+            <img
+              className={`${classes.iconPurchasedModal} ${classes.iconPurchasedModal}`}
+              src={pan}
+              alt="Rotating pan"
+            />
             <NavLink
               onClick={() => this.props.afterPurchase(this.props.orderStatus)}
               className={classes.toMainPage}
@@ -30,13 +37,16 @@ class PurchasedModal extends Component {
       } else {
         modalConent = (
           <Auxi>
-            <p>nie udało sie sory</p>
-            <NavLink
-              onClick={() => console.log("chuj")}
-              className={classes.toMainPage}
-              exact
-              to={"/"}
-            >
+            <h1 className={classes.ModalH}>
+              Niestety, transakcja nie powiodła się
+            </h1>
+            <p>Spróbuj ponownie za parę minut</p>
+            <img
+              className={`${classes.iconPurchasedModal} ${classes.nosignal}`}
+              src={nosignal}
+              alt="No signal"
+            />
+            <NavLink className={classes.toMainPage} exact to={"/"}>
               Strona główna
             </NavLink>
           </Auxi>
