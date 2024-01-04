80f120805f8bf8230549013b04e737b994dd16f2
Marcin Szaro <mszaro@gainsight.com>
2024-01-04 16:51:22 +0100

    Clean up index html and index js files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { createStore, applyMiddleware, compose } from "redux";
-import * as serviceWorker from "./serviceWorker";
 import thunk from "redux-thunk";
 import { BrowserRouter } from "react-router-dom";
 import mainReducer from "./store/reducers/main";
@@ -25,8 +24,3 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
