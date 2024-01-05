6e898fc36f2eb93674d1516896d2b3b5e6ed7275
Marcin Szaro <mszaro@gainsight.com>
2024-01-05 15:01:21 +0100

    Refactor Menu view

--- src/components/MenuPage/index.js
+++ src/components/MenuPage/index.js
@@ -1,48 +1,17 @@
 import React from "react";
-import classes from "./MenuPage.module.css";
-import Dish from "../smallParts/Dish";
-import Bar from "../smallParts/Bar";
+import { MenuContainer, Menu, Title, SubTitle } from "./styles";
+import { MainDishes, Soups, Desserts } from "./categories";
 
 const MenuPage = () => (
-  <section className={classes.Menu}>
-    <section className={classes.Container}>
-      <h1 className={classes.Title}>Menu</h1>
-      <p className={classes.subTitle}>Lorem ipsum dolor sit amet</p>
-      <h1 className={classes.categoryTitle}>Zupy</h1>
-      <Bar style={{ backgroundColor: "red" }} />
-      <div className={classes.DishesTable}>
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-      </div>
-      <h1 className={classes.categoryTitle}>Zupy</h1>
-      <Bar />
-      <div className={classes.DishesTable}>
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-      </div>
-      <h1 className={classes.categoryTitle}>Zupy</h1>
-      <Bar />
-      <div className={classes.DishesTable}>
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-        <Dish title="Kopytka" price="12" />
-      </div>
-    </section>
-  </section>
+  <MenuContainer>
+    <Menu>
+      <Title>Menu</Title>
+      <SubTitle>Lorem ipsum dolor sit amet</SubTitle>
+      <Soups />
+      <MainDishes />
+      <Desserts />
+    </Menu>
+  </MenuContainer>
 );
 
 export default MenuPage;
