c4ea6d70983f12fe9be7c8e6b7d0fabfcd65b410
Marcin Szaro <mszaro@gainsight.com>
2024-01-05 12:24:49 +0100

    Delete AsideIconBar

--- src/components/AsideIconBar/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import classes from "./AsideIconBar.module.css";
-
-const AsideIconBar = () => <div className={classes.AsideIconBar}></div>;
-
-export default AsideIconBar;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import AsideIconBar from "components/AsideIconBar";
 import NavBar from "components/NavBar";
 import { Main } from "./styles";
 
 const Layout = ({ children }) => (
   <>
     <NavBar />
-    <AsideIconBar />
     <Main>{children}</Main>
   </>
 );
