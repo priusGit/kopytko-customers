9dcbca2e10e01350da78546a2fa31e686d3b7986
Marcin Szaro <mszaro@gainsight.com>
2024-01-05 12:04:03 +0100

    Add MainPageAnchor

---
+++ src/components/MainPage/MainPageAnchor/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { MenuAnchor } from "./styles";
+
+const MainPageAnchor = () => <MenuAnchor href="/menu">Zobacz Menu</MenuAnchor>;
+
+export default MainPageAnchor;
